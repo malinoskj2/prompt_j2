1bb0ef742489ca1300e860ec929be17595395f49
Jesse Malinosky <j2@jessemalinosky.com>
2018-11-23 22:12:17 -0500

    adde test for prompt_j2_loader

---
+++ tests/test_loader.zsh
@@ -0,0 +1,14 @@
+#!/usr/bin/env zsh
+
+# tests for prompt_j2_loader
+
+# sh unit needs
+setopt shwordsplit
+SHUNIT_PARENT=$0
+
+source ../prompt_j2_loader
+
+# TESTS
+
+# run the tests
+source ../mod/shunit2/shunit2
